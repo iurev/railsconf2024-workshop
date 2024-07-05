3aa5d0761faa8def52ec62723b5ee2088469d3ca
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-05 20:24:39 +0100

    RUN_ID: 1

--- spec/lib/vacuum/media_attachments_vacuum_spec.rb
+++ spec/lib/vacuum/media_attachments_vacuum_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -7,19 +6,19 @@ RSpec.describe Vacuum::MediaAttachmentsVacuum do
   subject { described_class.new(retention_period) }
 
   let(:retention_period) { 7.days }
-  let(:remote_status) { Fabricate(:status, account: Fabricate(:account, domain: 'example.com')) }
-  let(:local_status) { Fabricate(:status) }
+  let_it_be(:remote_status) { Fabricate(:status, account: Fabricate(:account, domain: 'example.com')) }
+  let_it_be(:local_status) { Fabricate(:status) }
 
   describe '#perform' do
-    let!(:old_remote_media) { Fabricate(:media_attachment, remote_url: 'https://example.com/foo.png', status: remote_status, created_at: (retention_period + 1.day).ago, updated_at: (retention_period + 1.day).ago) }
-    let!(:old_local_media) { Fabricate(:media_attachment, status: local_status, created_at: (retention_period + 1.day).ago, updated_at: (retention_period + 1.day).ago) }
-    let!(:new_remote_media) { Fabricate(:media_attachment, remote_url: 'https://example.com/foo.png', status: remote_status) }
-    let!(:new_local_media) { Fabricate(:media_attachment, status: local_status) }
-    let!(:old_unattached_media) { Fabricate(:media_attachment, account_id: nil, created_at: 10.days.ago) }
-    let!(:new_unattached_media) { Fabricate(:media_attachment, account_id: nil, created_at: 1.hour.ago) }
+    let_it_be(:old_remote_media) { Fabricate(:media_attachment, remote_url: 'https://example.com/foo.png', status: remote_status, created_at: (7.days + 1.day).ago, updated_at: (7.days + 1.day).ago) }
+    let_it_be(:old_local_media) { Fabricate(:media_attachment, status: local_status, created_at: (7.days + 1.day).ago, updated_at: (7.days + 1.day).ago) }
+    let_it_be(:new_remote_media) { Fabricate(:media_attachment, remote_url: 'https://example.com/foo.png', status: remote_status) }
+    let_it_be(:new_local_media) { Fabricate(:media_attachment, status: local_status) }
+    let_it_be(:old_unattached_media) { Fabricate(:media_attachment, account_id: nil, created_at: 10.days.ago) }
+    let_it_be(:new_unattached_media) { Fabricate(:media_attachment, account_id: nil, created_at: 1.hour.ago) }
 
-    before do
-      subject.perform
+    before_all do
+      described_class.new(7.days).perform
     end
 
     it 'deletes cache of remote media attachments past the retention period' do
