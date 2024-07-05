07e16bc077b6af0a3f3845376be1c4dd9b8cc061
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-05 20:32:33 +0100

    RUN_ID: 1

--- spec/lib/vacuum/preview_cards_vacuum_spec.rb
+++ spec/lib/vacuum/preview_cards_vacuum_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -9,15 +8,15 @@ RSpec.describe Vacuum::PreviewCardsVacuum do
   let(:retention_period) { 7.days }
 
   describe '#perform' do
-    let!(:orphaned_preview_card) { Fabricate(:preview_card, created_at: 2.days.ago) }
-    let!(:old_preview_card) { Fabricate(:preview_card, updated_at: (retention_period + 1.day).ago) }
-    let!(:new_preview_card) { Fabricate(:preview_card) }
+    let_it_be(:orphaned_preview_card) { Fabricate(:preview_card, created_at: 2.days.ago) }
+    let_it_be(:old_preview_card) { Fabricate(:preview_card, updated_at: (retention_period + 1.day).ago) }
+    let_it_be(:new_preview_card) { Fabricate(:preview_card) }
 
-    before do
+    before_all do
       old_preview_card.statuses << Fabricate(:status)
       new_preview_card.statuses << Fabricate(:status)
 
-      subject.perform
+      described_class.new(7.days).perform
     end
 
     it 'deletes cache of preview cards last updated before the retention period' do
