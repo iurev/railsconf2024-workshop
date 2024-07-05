32815daa7f3360fbceea9e15b177a6b7982c2711
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-05 20:38:12 +0100

    RUN_ID: 3

--- spec/requests/api/v1/media_spec.rb
+++ spec/requests/api/v1/media_spec.rb
@@ -5,8 +5,8 @@ require 'rails_helper'
 RSpec.describe 'Media' do
   let_it_be(:user)    { Fabricate(:user) }
   let_it_be(:scopes)  { 'write:media' }
-  let_it_be(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
-  let_it_be(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+  let(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+  let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
   describe 'GET /api/v1/media/:id' do
     let_it_be(:media) { Fabricate(:media_attachment, account: user.account) }
