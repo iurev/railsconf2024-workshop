b4b975417491a9ac11a8bda6e956f44222e23387
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-09 17:23:32 +0100

    RUN_ID: 3

--- spec/services/backup_service_spec.rb
+++ spec/services/backup_service_spec.rb
@@ -1,16 +1,1 @@
-# spec/factories.rb
-
-FactoryBot.define do
-  factory :user do
-    # ... other attributes ...
-
-    # Use traits to build associations only when needed
-    trait :with_account do
-      after(:create) do |user|
-        create(:account, user: user)
-      end
-    end
-
-    # ... other traits ...
-  end
-end
+require 'rails_helper'
