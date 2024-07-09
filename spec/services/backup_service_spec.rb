3168f525200b865917a4eb4e9c82c87768559e81
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-09 16:43:10 +0100

    RUN_ID: 1

--- spec/services/backup_service_spec.rb
+++ spec/services/backup_service_spec.rb
@@ -1,46 +1,35 @@
-# frozen_string_literal: true
-# aiptimize started
-
 require 'rails_helper'
 
 RSpec.describe BackupService do
-  subject(:service_call) { described_class.new.call(backup) }
+  let_it_be(:user)           { Fabricate(:user) }
+  let_it_be(:attachment)     { Fabricate(:media_attachment, account: user.account) }
+  let_it_be(:status)         { Fabricate(:status, account: user.account, text: 'Hello', visibility: :public, media_attachments: [attachment]) }
+  let_it_be(:private_status) { Fabricate(:status, account: user.account, text: 'secret', visibility: :private) }
+  let_it_be(:favourite)      { Fabricate(:favourite, account: user.account) }
+  let_it_be(:bookmark)       { Fabricate(:bookmark, account: user.account) }
+  let_it_be(:backup)         { Fabricate(:backup, user: user) }
 
-  let!(:user)           { Fabricate(:user) }
-  let!(:attachment)     { Fabricate(:media_attachment, account: user.account) }
-  let!(:status)         { Fabricate(:status, account: user.account, text: 'Hello', visibility: :public, media_attachments: [attachment]) }
-  let!(:private_status) { Fabricate(:status, account: user.account, text: 'secret', visibility: :private) }
-  let!(:favourite)      { Fabricate(:favourite, account: user.account) }
-  let!(:bookmark)       { Fabricate(:bookmark, account: user.account) }
-  let!(:backup)         { Fabricate(:backup, user: user) }
-
-  def read_zip_file(backup, filename)
-    file = Paperclip.io_adapters.for(backup.dump)
-    Zip::File.open(file) do |zipfile|
-      entry = zipfile.glob(filename).first
-      return entry.get_input_stream.read
-    end
+  let_it_be(:avatar_path) do
+    user.account.update!(avatar: attachment_fixture('avatar.gif'))
+    user.account.avatar.url
   end
 
-  context 'when the user has an avatar and header' do
-    before do
-      user.account.update!(avatar: attachment_fixture('avatar.gif'))
-      user.account.update!(header: attachment_fixture('emojo.png'))
-    end
+  let_it_be(:header_path) do
+    user.account.update!(header: attachment_fixture('emojo.png'))
+    user.account.header.url
+  end
 
-    it 'stores them as expected' do
-      service_call
+  subject(:service_call) { described_class.new.call(backup) }
 
-      json = export_json(:actor)
-      avatar_path = json.dig('icon', 'url')
-      header_path = json.dig('image', 'url')
+  it 'stores them as expected', :aggregate_failures do
+    service_call
 
-      expect(avatar_path).to_not be_nil
-      expect(header_path).to_not be_nil
+    json = export_json(:actor)
+    expect(json.dig('icon', 'url')).to eq(avatar_path)
+    expect(json.dig('image', 'url')).to eq(header_path)
 
-      expect(read_zip_file(backup, avatar_path)).to be_present
-      expect(read_zip_file(backup, header_path)).to be_present
-    end
+    expect(read_zip_file(backup, avatar_path)).to be_present
+    expect(read_zip_file(backup, header_path)).to be_present
   end
 
   it 'marks the backup as processed and exports files' do
