7f06a0d8c2692c02f54e9761de5cc759263d9bca
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 00:46:09 +0100

    RUN_ID: 2

--- spec/controllers/admin/account_moderation_notes_controller_spec.rb
+++ spec/controllers/admin/account_moderation_notes_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Admin::AccountModerationNotesController do
   let_it_be(:user) { Fabricate(:user, role: UserRole.find_by(name: 'Admin')) }
   let_it_be(:target_account) { Fabricate(:account) }
 
-  before(:all) do
+  before do
     sign_in user, scope: :user
   end
 
