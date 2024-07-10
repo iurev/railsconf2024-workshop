bd7b37aaf5032824d557462cc4c84be6293c65ad
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-10 18:41:42 +0100

    RUN_ID: 1

--- spec/lib/mastodon/cli/upgrade_spec.rb
+++ spec/lib/mastodon/cli/upgrade_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 require 'mastodon/cli/upgrade'
@@ -7,7 +6,7 @@ require 'mastodon/cli/upgrade'
 describe Mastodon::CLI::Upgrade do
   subject { cli.invoke(action, arguments, options) }
 
-  let(:cli) { described_class.new }
+  let_it_be(:cli) { described_class.new }
   let(:arguments) { [] }
   let(:options) { {} }
 
