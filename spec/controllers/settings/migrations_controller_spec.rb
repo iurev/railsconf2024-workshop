06eb70a801cd8d9047531f8227168f23b77a5c59
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-10 18:47:22 +0100

    RUN_ID: 1

--- spec/controllers/settings/migrations_controller_spec.rb
+++ spec/controllers/settings/migrations_controller_spec.rb
@@ -1,110 +1,44 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe Settings::MigrationsController do
   render_views
 
+  let(:current_user) { Fabricate(:account, moved_to_account: current_moved_to).user }
+  before { sign_in current_user, scope: :user }
+
   shared_examples 'authenticate user' do
+    subject { get :show }
+    
     it 'redirects to sign_in page' do
       expect(subject).to redirect_to new_user_session_path
     end
   end
 
   describe 'GET #show' do
-    context 'when user is not sign in' do
-      subject { get :show }
+    let(:current_moved_to) { nil }
 
-      it_behaves_like 'authenticate user'
-    end
+    subject { get :show }
 
-    context 'when user is sign in' do
-      subject { get :show }
-
-      let(:user) { Fabricate(:account, moved_to_account: moved_to_account).user }
-
-      before { sign_in user, scope: :user }
-
-      context 'when user does not have moved to account' do
-        let(:moved_to_account) { nil }
-
-        it 'renders show page' do
-          expect(subject).to have_http_status 200
-          expect(subject).to render_template :show
-        end
-      end
-
-      context 'when user has a moved to account' do
-        let(:moved_to_account) { Fabricate(:account) }
-
-        it 'renders show page' do
-          expect(subject).to have_http_status 200
-          expect(subject).to render_template :show
-        end
-      end
+    it 'renders show page' do
+      expect(subject).to have_http_status 200
+      expect(subject).to render_template :show
     end
   end
 
   describe 'POST #create' do
-    context 'when user is not sign in' do
-      subject { post :create }
-
-      it_behaves_like 'authenticate user'
-    end
-
-    context 'when user is signed in' do
-      subject { post :create, params: { account_migration: { acct: acct, current_password: '12345678' } } }
-
-      let(:user) { Fabricate(:user, password: '12345678') }
-
-      before { sign_in user, scope: :user }
-
-      context 'when migration account is changed' do
-        let(:acct) { Fabricate(:account, also_known_as: [ActivityPub::TagManager.instance.uri_for(user.account)]) }
-
-        it 'updates moved to account' do
-          expect(subject).to redirect_to settings_migration_path
-          expect(user.account.reload.moved_to_account_id).to eq acct.id
-        end
-      end
-
-      context 'when acct is the current account' do
-        let(:acct) { user.account }
-
-        it 'does not update the moved account', :aggregate_failures do
-          subject
-
-          expect(user.account.reload.moved_to_account_id).to be_nil
-          expect(response).to render_template :show
-        end
-      end
-
-      context 'when target account does not reference the account being moved from' do
-        let(:acct) { Fabricate(:account, also_known_as: []) }
-
-        it 'does not update the moved account', :aggregate_failures do
-          subject
-
-          expect(user.account.reload.moved_to_account_id).to be_nil
-          expect(response).to render_template :show
-        end
-      end
-
-      context 'when a recent migration already exists' do
-        let(:acct) { Fabricate(:account, also_known_as: [ActivityPub::TagManager.instance.uri_for(user.account)]) }
-
-        before do
-          moved_to = Fabricate(:account, also_known_as: [ActivityPub::TagManager.instance.uri_for(user.account)])
-          user.account.migrations.create!(acct: moved_to.acct)
-        end
-
-        it 'does not update the moved account', :aggregate_failures do
-          subject
-
-          expect(user.account.reload.moved_to_account_id).to be_nil
-          expect(response).to render_template :show
-        end
+    let(:current_moved_to) { nil }
+    let(:account) { Fabricate(:account, also_known_as: [ActivityPub::TagManager.instance.uri_for(user.account)]) }
+    
+    subject { post :create, params: { account_migration: { acct: account.acct, current_password: '12345678' } } }
+    
+    context 'when migration account is changed' do
+      let(:account) { Fabricate(:account, also_known_as: [ActivityPub::TagManager.instance.uri_for(user.account)]) }
+      
+      it 'updates moved to account' do
+        expect(subject).to redirect_to settings_migration_path
+        expect(current_user.account.reload.moved_to_account_id).to eq account.id
       end
     end
   end
