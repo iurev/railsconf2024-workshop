5bf03cbfae03f3178227362e72a485ad5022502a
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-10 17:24:25 +0100

    RUN_ID: 2

--- spec/requests/api/v1/bookmarks_spec.rb
+++ spec/requests/api/v1/bookmarks_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe 'Bookmarks' do
   let_it_be(:user)    { Fabricate(:user) }
-  let_it_be(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
-  let_it_be(:scopes)  { 'read:bookmarks' }
+  let(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+  let(:scopes)  { 'read:bookmarks' }
   let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
   describe 'GET /api/v1/bookmarks' do
