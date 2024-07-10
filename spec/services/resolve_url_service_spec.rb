d492bcb6a6bb113a96c580bd07ee76ee0a37e528
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-10 18:22:34 +0100

    RUN_ID: 3

--- spec/services/resolve_url_service_spec.rb
+++ spec/services/resolve_url_service_spec.rb
@@ -5,6 +5,10 @@ require 'rails_helper'
 describe ResolveURLService do
   subject { described_class.new }
 
+  let_it_be(:account) { Fabricate(:account) }
+  let_it_be(:poster)  { Fabricate(:account, domain: 'example.com') }
+  let_it_be(:status)  { Fabricate(:status, account: poster, visibility: :private) }
+
   describe '#call' do
     it 'returns nil when there is no resource url' do
       url = 'http://example.com/missing-resource'
@@ -30,83 +34,11 @@ describe ResolveURLService do
       expect(subject.call(url)).to eq known_account
     end
 
-    shared_context 'with remote private status' do
-      let_it_be(:account) { Fabricate(:account) }
-      let_it_be(:poster)  { Fabricate(:account, domain: 'example.com') }
-      let_it_be(:status)  { Fabricate(:status, account: poster, visibility: :private) }
-
+    shared_examples 'remote private status' do
       before do
         stub_request(:get, url).to_return(status: 404) if defined?(url)
         stub_request(:get, uri).to_return(status: 404) if defined?(uri)
       end
-    end
-
-    context 'when searching for a remote private status' do
-      include_context 'with remote private status'
-
-      context 'when the account follows the poster' do
-        before do
-          account.follow!(poster)
-        end
-
-        context 'when the status uses Mastodon-style URLs' do
-          let(:url) { 'https://example.com/@foo/42' }
-          let(:uri) { 'https://example.com/users/foo/statuses/42' }
-
-          it 'returns status by url' do
-            status.update!(url: url, uri: uri)
-            expect(subject.call(url, on_behalf_of: account)).to eq(status)
-          end
-
-          it 'returns status by uri' do
-            status.update!(url: url, uri: uri)
-            expect(subject.call(uri, on_behalf_of: account)).to eq(status)
-          end
-        end
-
-        context 'when the status uses pleroma-style URLs' do
-          let(:uri) { 'https://example.com/objects/0123-456-789-abc-def' }
-
-          it 'returns status by uri' do
-            status.update!(url: nil, uri: uri)
-            expect(subject.call(uri, on_behalf_of: account)).to eq(status)
-          end
-        end
-      end
-
-      context 'when the account does not follow the poster' do
-        context 'when the status uses Mastodon-style URLs' do
-          let(:url) { 'https://example.com/@foo/42' }
-          let(:uri) { 'https://example.com/users/foo/statuses/42' }
-
-          it 'does not return the status by url' do
-            status.update!(url: url, uri: uri)
-            expect(subject.call(url, on_behalf_of: account)).to be_nil
-          end
-
-          it 'does not return the status by uri' do
-            status.update!(url: url, uri: uri)
-            expect(subject.call(uri, on_behalf_of: account)).to be_nil
-          end
-        end
-
-        context 'when the status uses pleroma-style URLs' do
-          let(:uri) { 'https://example.com/objects/0123-456-789-abc-def' }
-
-          it 'returns status by uri' do
-            status.update!(url: nil, uri: uri)
-            expect(subject.call(uri, on_behalf_of: account)).to be_nil
-          end
-        end
-      end
-    end
-
-    context 'when searching for a local private status' do
-      let_it_be(:account) { Fabricate(:account) }
-      let_it_be(:poster)  { Fabricate(:account) }
-      let_it_be(:status)  { Fabricate(:status, account: poster, visibility: :private) }
-      let(:url)     { ActivityPub::TagManager.instance.url_for(status) }
-      let(:uri)     { ActivityPub::TagManager.instance.uri_for(status) }
 
       context 'when the account follows the poster' do
         before do
@@ -133,29 +65,55 @@ describe ResolveURLService do
       end
     end
 
-    context 'when searching for a link that redirects to a local public status' do
-      let_it_be(:account) { Fabricate(:account) }
-      let_it_be(:poster)  { Fabricate(:account) }
-      let_it_be(:status)  { Fabricate(:status, account: poster, visibility: :public) }
-      let(:url)     { 'https://link.to/foobar' }
-      let(:status_url) { ActivityPub::TagManager.instance.url_for(status) }
+    context 'when searching for a remote private status' do
+      context 'when the status uses Mastodon-style URLs' do
+        let(:url) { 'https://example.com/@foo/42' }
+        let(:uri) { 'https://example.com/users/foo/statuses/42' }
+
+        before do
+          status.update!(url: url, uri: uri)
+        end
+
+        include_examples 'remote private status'
+      end
+
+      context 'when the status uses pleroma-style URLs' do
+        let(:uri) { 'https://example.com/objects/0123-456-789-abc-def' }
+
+        before do
+          status.update!(url: nil, uri: uri)
+        end
+
+        include_examples 'remote private status'
+      end
+    end
+
+    context 'when searching for a local private status' do
+      let(:url) { ActivityPub::TagManager.instance.url_for(status) }
       let(:uri) { ActivityPub::TagManager.instance.uri_for(status) }
+
+      include_examples 'remote private status'
+    end
+
+    context 'when searching for a link that redirects to a local public status' do
+      let_it_be(:public_status) { Fabricate(:status, account: poster, visibility: :public) }
+      let(:url)     { 'https://link.to/foobar' }
+      let(:status_url) { ActivityPub::TagManager.instance.url_for(public_status) }
+      let(:uri) { ActivityPub::TagManager.instance.uri_for(public_status) }
 
       before do
         stub_request(:get, url).to_return(status: 302, headers: { 'Location' => status_url })
-        body = ActiveModelSerializers::SerializableResource.new(status, serializer: ActivityPub::NoteSerializer, adapter: ActivityPub::Adapter).to_json
+        body = ActiveModelSerializers::SerializableResource.new(public_status, serializer: ActivityPub::NoteSerializer, adapter: ActivityPub::Adapter).to_json
         stub_request(:get, status_url).to_return(body: body, headers: { 'Content-Type' => 'application/activity+json' })
         stub_request(:get, uri).to_return(body: body, headers: { 'Content-Type' => 'application/activity+json' })
       end
 
       it 'returns status by url' do
-        expect(subject.call(url, on_behalf_of: account)).to eq(status)
+        expect(subject.call(url, on_behalf_of: account)).to eq(public_status)
       end
     end
 
     context 'when searching for a local link of a remote private status' do
-      include_context 'with remote private status'
-
       let(:url)        { 'https://example.com/@foo/42' }
       let(:uri)        { 'https://example.com/users/foo/statuses/42' }
       let(:search_url) { "https://#{Rails.configuration.x.local_domain}/@foo@example.com/#{status.id}" }
@@ -164,21 +122,7 @@ describe ResolveURLService do
         status.update!(url: url, uri: uri)
       end
 
-      context 'when the account follows the poster' do
-        before do
-          account.follow!(poster)
-        end
-
-        it 'returns the status' do
-          expect(subject.call(search_url, on_behalf_of: account)).to eq(status)
-        end
-      end
-
-      context 'when the account does not follow the poster' do
-        it 'does not return the status' do
-          expect(subject.call(search_url, on_behalf_of: account)).to be_nil
-        end
-      end
+      include_examples 'remote private status'
     end
   end
 end
