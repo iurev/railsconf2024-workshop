688810a465af712aced4f7ccdef112495d22c81a
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-10 18:39:28 +0100

    RUN_ID: 1

--- spec/controllers/admin/report_notes_controller_spec.rb
+++ spec/controllers/admin/report_notes_controller_spec.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe Admin::ReportNotesController do
   render_views
 
-  let(:user) { Fabricate(:user, role: UserRole.find_by(name: 'Admin')) }
+  let_it_be(:user) { Fabricate(:user, role: UserRole.find_by(name: 'Admin')) }
 
-  before do
-    sign_in user, scope: :user
+  before_all do
+    RSpec::Mocks.with_temporary_scope do
+      sign_in user, scope: :user
+    end
   end
 
   describe 'POST #create' do
     subject { post :create, params: params }
 
-    let(:report) { Fabricate(:report, action_taken_at: action_taken, action_taken_by_account_id: account_id) }
+    let_it_be(:report) { Fabricate(:report) }
 
     context 'when parameter is valid' do
       context 'when report is unsolved' do
-        let(:action_taken) { nil }
-        let(:account_id) { nil }
+        before { report.update(action_taken_at: nil, action_taken_by_account_id: nil) }
 
         context 'when create_and_resolve flag is on' do
           let(:params) { { report_note: { content: 'test content', report_id: report.id }, create_and_resolve: nil } }
@@ -44,8 +44,7 @@ describe Admin::ReportNotesController do
       end
 
       context 'when report is resolved' do
-        let(:action_taken) { Time.now.utc }
-        let(:account_id) { user.account.id }
+        before { report.update(action_taken_at: Time.now.utc, action_taken_by_account_id: user.account.id) }
 
         context 'when create_and_unresolve flag is on' do
           let(:params) { { report_note: { content: 'test content', report_id: report.id }, create_and_unresolve: nil } }
@@ -71,8 +70,6 @@ describe Admin::ReportNotesController do
 
     context 'when parameter is invalid' do
       let(:params) { { report_note: { content: '', report_id: report.id } } }
-      let(:action_taken) { nil }
-      let(:account_id) { nil }
 
       it 'renders admin/reports/show' do
         expect(subject).to render_template 'admin/reports/show'
@@ -83,7 +80,7 @@ describe Admin::ReportNotesController do
   describe 'DELETE #destroy' do
     subject { delete :destroy, params: { id: report_note.id } }
 
-    let!(:report_note) { Fabricate(:report_note) }
+    let_it_be(:report_note) { Fabricate(:report_note) }
 
     it 'deletes note' do
       expect { subject }.to change(ReportNote, :count).by(-1)
