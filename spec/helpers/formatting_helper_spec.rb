07f47e8387f821665e28c167b60ed2936f2e1c85
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-10 18:28:04 +0100

    RUN_ID: 1

--- spec/helpers/formatting_helper_spec.rb
+++ spec/helpers/formatting_helper_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -7,7 +6,8 @@ describe FormattingHelper do
   include Devise::Test::ControllerHelpers
 
   describe '#rss_status_content_format' do
-    let(:status) { Fabricate(:status, text: 'Hello world<>', spoiler_text: 'This is a spoiler<>', poll: Fabricate(:poll, options: %w(Yes<> No))) }
+    let_it_be(:poll) { Fabricate(:poll, options: %w(Yes<> No)) }
+    let_it_be(:status) { Fabricate(:status, text: 'Hello world<>', spoiler_text: 'This is a spoiler<>', poll: poll) }
     let(:html) { helper.rss_status_content_format(status) }
 
     it 'renders the spoiler text' do
