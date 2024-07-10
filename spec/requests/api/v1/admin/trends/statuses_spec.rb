89d03b6bc2bcf5342449626021caadf5b40f3063
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-10 20:47:35 +0100

    RUN_ID: 1

--- spec/requests/api/v1/admin/trends/statuses_spec.rb
+++ spec/requests/api/v1/admin/trends/statuses_spec.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe 'API V1 Admin Trends Statuses' do
-  let(:role)   { UserRole.find_by(name: 'Admin') }
-  let(:user)   { Fabricate(:user, role: role) }
-  let(:scopes) { 'admin:read admin:write' }
-  let(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
-  let(:account) { Fabricate(:account) }
-  let(:status)  { Fabricate(:status) }
-  let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+  let_it_be(:role)   { UserRole.find_by(name: 'Admin') }
+  let_it_be(:user)   { Fabricate(:user, role: role) }
+  let_it_be(:scopes) { 'admin:read admin:write' }
+  let_it_be(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+  let_it_be(:account) { Fabricate(:account) }
+  let_it_be(:status)  { Fabricate(:status) }
+
+  before_all do
+    @headers = { 'Authorization' => "Bearer #{token.token}" }
+  end
 
   describe 'GET /api/v1/admin/trends/statuses' do
     it 'returns http success' do
-      get '/api/v1/admin/trends/statuses', params: { account_id: account.id, limit: 2 }, headers: headers
+      get '/api/v1/admin/trends/statuses', params: { account_id: account.id, limit: 2 }, headers: @headers
 
       expect(response).to have_http_status(200)
     end
@@ -22,7 +24,7 @@ describe 'API V1 Admin Trends Statuses' do
 
   describe 'POST /api/v1/admin/trends/statuses/:id/approve' do
     before do
-      post "/api/v1/admin/trends/statuses/#{status.id}/approve", headers: headers
+      post "/api/v1/admin/trends/statuses/#{status.id}/approve", headers: @headers
     end
 
     it_behaves_like 'forbidden for wrong scope', 'write:statuses'
@@ -35,7 +37,7 @@ describe 'API V1 Admin Trends Statuses' do
 
   describe 'POST /api/v1/admin/trends/statuses/:id/unapprove' do
     before do
-      post "/api/v1/admin/trends/statuses/#{status.id}/reject", headers: headers
+      post "/api/v1/admin/trends/statuses/#{status.id}/reject", headers: @headers
     end
 
     it_behaves_like 'forbidden for wrong scope', 'write:statuses'
