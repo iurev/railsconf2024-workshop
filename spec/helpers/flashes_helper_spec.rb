f16428db0a5d126412376891374c862c8394cd6c
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 11:36:42 +0100

    RUN_ID: 2

--- spec/helpers/flashes_helper_spec.rb
+++ spec/helpers/flashes_helper_spec.rb
@@ -4,19 +4,13 @@ require 'rails_helper'
 
 describe FlashesHelper do
   describe 'user_facing_flashes' do
-    let_it_be(:flash) do
-      {
-        alert: 'an alert',
-        error: 'an error',
-        notice: 'a notice',
-        success: 'a success',
-        not_user_facing: 'a not user facing flash'
-      }
-    end
-
     before do
       # rubocop:disable Rails/I18nLocaleTexts
-      allow(helper).to receive(:flash).and_return(flash)
+      flash[:alert] = 'an alert'
+      flash[:error] = 'an error'
+      flash[:notice] = 'a notice'
+      flash[:success] = 'a success'
+      flash[:not_user_facing] = 'a not user facing flash'
       # rubocop:enable Rails/I18nLocaleTexts
     end
 
