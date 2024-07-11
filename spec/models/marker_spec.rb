12e521610a23593940746f4a77744928746f57bf
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 21:03:49 +0100

    RUN_ID: 2

--- spec/models/marker_spec.rb
+++ spec/models/marker_spec.rb
@@ -3,15 +3,13 @@
 require 'rails_helper'
 
 describe Marker do
-  let_it_be(:marker) { described_class.new }
-
   describe 'validations' do
     describe 'timeline' do
       it 'must be included in valid list' do
-        marker.timeline = 'not real timeline'
+        record = described_class.new(timeline: 'not real timeline')
 
-        expect(marker).to_not be_valid
-        expect(marker).to model_have_error_on_field(:timeline)
+        expect(record).to_not be_valid
+        expect(record).to model_have_error_on_field(:timeline)
       end
     end
   end
