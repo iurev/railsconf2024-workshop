a2b533445d434c487b1418e77a2ea4faf5a1452c
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 08:38:46 +0100

    RUN_ID: 1

--- spec/features/admin/software_updates_spec.rb
+++ spec/features/admin/software_updates_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe 'finding software updates through the admin interface' do
-  before do
-    Fabricate(:software_update, version: '99.99.99', type: 'major', urgent: true, release_notes: 'https://github.com/mastodon/mastodon/releases/v99')
+  let_it_be(:software_update) { Fabricate(:software_update, version: '99.99.99', type: 'major', urgent: true, release_notes: 'https://github.com/mastodon/mastodon/releases/v99') }
+  let_it_be(:user) { Fabricate(:user, role: UserRole.find_by(name: 'Owner')) }
 
-    sign_in Fabricate(:user, role: UserRole.find_by(name: 'Owner')), scope: :user
+  before do
+    sign_in user, scope: :user
   end
 
   it 'shows a link to the software updates page, which links to release notes' do
