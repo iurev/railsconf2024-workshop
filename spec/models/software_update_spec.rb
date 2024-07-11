fd9c899f1789a5736dbc844baec8a904748ac8f9
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 22:25:42 +0100

    RUN_ID: 1

--- spec/models/software_update_spec.rb
+++ spec/models/software_update_spec.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 RSpec.describe SoftwareUpdate do
   describe '.pending_to_a' do
+    let_it_be(:patch_update) { Fabricate(:software_update, version: '3.4.42', type: 'patch', urgent: true) }
+    let_it_be(:minor_update) { Fabricate(:software_update, version: '3.5.0', type: 'minor', urgent: false) }
+    let_it_be(:major_update) { Fabricate(:software_update, version: '4.2.0', type: 'major', urgent: false) }
+
     before do
       allow(Mastodon::Version).to receive(:gem_version).and_return(Gem::Version.new(mastodon_version))
-
-      Fabricate(:software_update, version: '3.4.42', type: 'patch', urgent: true)
-      Fabricate(:software_update, version: '3.5.0', type: 'minor', urgent: false)
-      Fabricate(:software_update, version: '4.2.0', type: 'major', urgent: false)
     end
 
     context 'when the Mastodon version is an outdated release' do
