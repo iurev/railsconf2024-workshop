f7fdb1f9f5cdf6c8be25006dbfd5a5793a85327f
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 05:19:37 +0100

    RUN_ID: 1

--- spec/controllers/media_proxy_controller_spec.rb
+++ spec/controllers/media_proxy_controller_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -10,17 +9,18 @@ describe MediaProxyController do
     stub_request(:get, 'http://example.com/attachment.png').to_return(request_fixture('avatar.txt'))
   end
 
+  let_it_be(:status) { Fabricate(:status) }
+  let_it_be(:media_attachment) { Fabricate(:media_attachment, status: status, remote_url: 'http://example.com/attachment.png') }
+
   describe '#show' do
     it 'redirects when attached to a status' do
-      status = Fabricate(:status)
-      media_attachment = Fabricate(:media_attachment, status: status, remote_url: 'http://example.com/attachment.png')
       get :show, params: { id: media_attachment.id }
 
       expect(response).to have_http_status(302)
     end
 
     it 'responds with missing when there is not an attached status' do
-      media_attachment = Fabricate(:media_attachment, status: nil, remote_url: 'http://example.com/attachment.png')
+      media_attachment.update!(status: nil)
       get :show, params: { id: media_attachment.id }
 
       expect(response).to have_http_status(404)
@@ -33,9 +33,9 @@ describe MediaProxyController do
     end
 
     it 'raises when not permitted to view' do
-      status = Fabricate(:status, visibility: :direct)
-      media_attachment = Fabricate(:media_attachment, status: status, remote_url: 'http://example.com/attachment.png')
-      get :show, params: { id: media_attachment.id }
+      direct_status = Fabricate(:status, visibility: :direct)
+      direct_media_attachment = Fabricate(:media_attachment, status: direct_status, remote_url: 'http://example.com/attachment.png')
+      get :show, params: { id: direct_media_attachment.id }
 
       expect(response).to have_http_status(404)
     end
