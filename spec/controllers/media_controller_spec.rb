be1fab78cd2d13bad90473e65d627cddb52d9061
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 05:18:41 +0100

    RUN_ID: 1

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe MediaController do
   render_views
+
+  let_it_be(:account) { Fabricate(:account) }
+  let_it_be(:status) { Fabricate(:status, account: account) }
+  let_it_be(:media_attachment) { Fabricate(:media_attachment, status: status, shortcode: 'OI6IgDzG-nYTqvDQ994') }
 
   describe '#show' do
     it 'raises when shortcode cant be found' do
@@ -15,23 +18,20 @@ describe MediaController do
 
     context 'when the media attachment has a shortcode' do
       it 'redirects to the file url when attached to a status' do
-        status = Fabricate(:status)
-        media_attachment = Fabricate(:media_attachment, status: status, shortcode: 'OI6IgDzG-nYTqvDQ994')
         get :show, params: { id: media_attachment.to_param }
 
         expect(response).to redirect_to(media_attachment.file.url(:original))
       end
 
       it 'responds with missing when there is not an attached status' do
-        media_attachment = Fabricate(:media_attachment, status: nil, shortcode: 'OI6IgDzG-nYTqvDQ994')
+        media_attachment.update!(status: nil)
         get :show, params: { id: media_attachment.to_param }
 
         expect(response).to have_http_status(404)
       end
 
       it 'raises when not permitted to view' do
-        status = Fabricate(:status, visibility: :direct)
-        media_attachment = Fabricate(:media_attachment, status: status, shortcode: 'OI6IgDzG-nYTqvDQ994')
+        status.update!(visibility: :direct)
         get :show, params: { id: media_attachment.to_param }
 
         expect(response).to have_http_status(404)
@@ -39,24 +39,23 @@ describe MediaController do
     end
 
     context 'when the media attachment has no shortcode' do
+      before { media_attachment.update!(shortcode: nil) }
+
       it 'redirects to the file url when attached to a status' do
-        status = Fabricate(:status)
-        media_attachment = Fabricate(:media_attachment, status: status)
         get :show, params: { id: media_attachment.to_param }
 
         expect(response).to redirect_to(media_attachment.file.url(:original))
       end
 
       it 'responds with missing when there is not an attached status' do
-        media_attachment = Fabricate(:media_attachment, status: nil)
+        media_attachment.update!(status: nil)
         get :show, params: { id: media_attachment.to_param }
 
         expect(response).to have_http_status(404)
       end
 
       it 'raises when not permitted to view' do
-        status = Fabricate(:status, visibility: :direct)
-        media_attachment = Fabricate(:media_attachment, status: status)
+        status.update!(visibility: :direct)
         get :show, params: { id: media_attachment.to_param }
 
         expect(response).to have_http_status(404)
