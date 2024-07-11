096ae1ce75cc37e6dd224af2f6cb7c209095bc5a
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 07:58:03 +0100

    RUN_ID: 2

--- spec/fabrication/fabricators_spec.rb
+++ spec/fabrication/fabricators_spec.rb
@@ -3,16 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe "Fabricators", :fabrication do
-  before_all do
+  before(:all) do
     Fabrication.manager.load_definitions if Fabrication.manager.empty?
   end
 
-  let_it_be(:factory_names) { Fabrication.manager.schematics.map(&:first) }
-
-  factory_names.each do |factory_name|
-    it "is able to create valid records for #{factory_name}" do
-      records = Fabricate.times(2, factory_name)
-      expect(records).to all(be_valid)
+  Fabrication.manager.schematics.map(&:first).each do |factory_name|
+    describe "The #{factory_name} factory" do
+      it 'is able to create valid records' do
+        records = Fabricate.times(2, factory_name)
+        expect(records).to all(be_valid)
+      end
     end
   end
 end
