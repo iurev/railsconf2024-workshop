036f1b91e6ae5a5531b5fd9aa92d2f9e4adfe8aa
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 06:50:07 +0100

    RUN_ID: 1

--- spec/controllers/settings/login_activities_controller_spec.rb
+++ spec/controllers/settings/login_activities_controller_spec.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe Settings::LoginActivitiesController do
   render_views
 
-  let!(:user) { Fabricate(:user) }
-  let!(:login_activity) { Fabricate :login_activity, user: user }
+  let_it_be(:user) { Fabricate(:user) }
+  let_it_be(:login_activity) { Fabricate :login_activity, user: user }
 
-  before do
+  before_all do
     sign_in user, scope: :user
   end
 
