253f3df6b7539634739e1146c306f1ac2fe713f9
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 19:43:05 +0100

    RUN_ID: 2

--- spec/models/custom_filter_spec.rb
+++ spec/models/custom_filter_spec.rb
@@ -3,36 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe CustomFilter do
-  let_it_be(:custom_filter) { described_class.new }
-
   describe 'Validations' do
     it 'requires presence of title' do
-      record = custom_filter.dup
-      record.title = ''
+      record = described_class.new(title: '')
       record.valid?
 
       expect(record).to model_have_error_on_field(:title)
     end
 
     it 'requires presence of context' do
-      record = custom_filter.dup
-      record.context = nil
+      record = described_class.new(context: nil)
       record.valid?
 
       expect(record).to model_have_error_on_field(:context)
     end
 
     it 'requires non-empty of context' do
-      record = custom_filter.dup
-      record.context = []
+      record = described_class.new(context: [])
       record.valid?
 
       expect(record).to model_have_error_on_field(:context)
     end
 
     it 'requires valid context value' do
-      record = custom_filter.dup
-      record.context = ['invalid']
+      record = described_class.new(context: ['invalid'])
       record.valid?
 
       expect(record).to model_have_error_on_field(:context)
@@ -41,8 +35,7 @@ RSpec.describe CustomFilter do
 
   describe 'Normalizations' do
     it 'cleans up context values' do
-      record = custom_filter.dup
-      record.context = ['home', 'notifications', 'public    ', '']
+      record = described_class.new(context: ['home', 'notifications', 'public    ', ''])
 
       expect(record.context).to eq(%w(home notifications public))
     end
