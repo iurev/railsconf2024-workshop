2806b2ffe26fea87d1c139de2085a0c07981d281
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 16:21:28 +0100

    RUN_ID: 2

--- spec/lib/mastodon/cli/search_spec.rb
+++ spec/lib/mastodon/cli/search_spec.rb
@@ -6,7 +6,7 @@ require 'mastodon/cli/search'
 describe Mastodon::CLI::Search do
   subject { cli.invoke(action, arguments, options) }
 
-  let_it_be(:cli) { described_class.new }
+  let(:cli) { described_class.new }
   let(:arguments) { [] }
   let(:options) { {} }
 
