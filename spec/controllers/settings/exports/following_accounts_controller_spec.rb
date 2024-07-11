c6b956c9267ff73db00e01a91a6ca4e9dd30c212
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 06:10:01 +0100

    RUN_ID: 1

--- spec/controllers/settings/exports/following_accounts_controller_spec.rb
+++ spec/controllers/settings/exports/following_accounts_controller_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -7,11 +6,15 @@ describe Settings::Exports::FollowingAccountsController do
   render_views
 
   describe 'GET #index' do
-    it 'returns a csv of the following accounts' do
-      user = Fabricate(:user)
-      user.account.follow!(Fabricate(:account, username: 'username', domain: 'domain'))
+    let_it_be(:user) { Fabricate(:user) }
+    let_it_be(:followed_account) { Fabricate(:account, username: 'username', domain: 'domain') }
 
+    before do
+      user.account.follow!(followed_account)
       sign_in user, scope: :user
+    end
+
+    it 'returns a csv of the following accounts' do
       get :index, format: :csv
 
       expect(response.body).to eq "Account address,Show boosts,Notify on new posts,Languages\nusername@domain,true,false,\n"
