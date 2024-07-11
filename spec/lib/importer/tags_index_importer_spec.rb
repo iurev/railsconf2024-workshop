5a686ad7a94b515bdaf68f501e92532f8e0d3978
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 15:38:17 +0100

    RUN_ID: 1

--- spec/lib/importer/tags_index_importer_spec.rb
+++ spec/lib/importer/tags_index_importer_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -8,7 +7,7 @@ describe Importer::TagsIndexImporter do
     let(:pool) { Concurrent::FixedThreadPool.new(5) }
     let(:importer) { described_class.new(batch_size: 123, executor: pool) }
 
-    before { Fabricate(:tag) }
+    let_it_be(:tag) { Fabricate(:tag) }
 
     it 'indexes relevant tags' do
       expect { importer.import! }.to update_index(TagsIndex)
