cd98e69417d62e33abb6c91d30ed79ed303d4138
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 21:03:41 +0100

    RUN_ID: 1

--- spec/models/marker_spec.rb
+++ spec/models/marker_spec.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe Marker do
+  let_it_be(:marker) { described_class.new }
+
   describe 'validations' do
     describe 'timeline' do
       it 'must be included in valid list' do
-        record = described_class.new(timeline: 'not real timeline')
+        marker.timeline = 'not real timeline'
 
-        expect(record).to_not be_valid
-        expect(record).to model_have_error_on_field(:timeline)
+        expect(marker).to_not be_valid
+        expect(marker).to model_have_error_on_field(:timeline)
       end
     end
   end
