885b95b03dd8a289be1bedf1a35267280322b66c
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 15:35:34 +0100

    RUN_ID: 1

--- spec/lib/importer/base_importer_spec.rb
+++ spec/lib/importer/base_importer_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe Importer::BaseImporter do
   describe 'import!' do
-    let(:pool) { Concurrent::FixedThreadPool.new(5) }
-    let(:importer) { described_class.new(batch_size: 123, executor: pool) }
+    let_it_be(:pool) { Concurrent::FixedThreadPool.new(5) }
+    let_it_be(:importer) { described_class.new(batch_size: 123, executor: pool) }
 
     it 'raises an error' do
       expect { importer.import! }.to raise_error(NotImplementedError)
