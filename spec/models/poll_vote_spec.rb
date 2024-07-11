2c3781af7d9a1bc5e6d2b8bd95149b2f358041d8
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 21:10:30 +0100

    RUN_ID: 2

--- spec/models/poll_vote_spec.rb
+++ spec/models/poll_vote_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe PollVote do
   describe '#object_type' do
-    let_it_be(:poll_vote) { Fabricate.build(:poll_vote) }
+    let(:poll_vote) { Fabricate.build(:poll_vote) }
 
     it 'returns :vote' do
       expect(poll_vote.object_type).to eq :vote
@@ -13,8 +13,8 @@ RSpec.describe PollVote do
 
   describe 'validations' do
     context 'with a vote on an expired poll' do
-      let_it_be(:poll) { Fabricate.build(:poll, expires_at: 30.days.ago) }
-      let_it_be(:vote) { Fabricate.build(:poll_vote, poll: poll) }
+      let(:poll) { Fabricate(:poll, expires_at: 30.days.ago) }
+      let(:vote) { Fabricate.build(:poll_vote, poll: poll) }
 
       it 'marks the vote invalid' do
         expect(vote).to_not be_valid
@@ -22,7 +22,7 @@ RSpec.describe PollVote do
     end
 
     context 'with invalid choices' do
-      let_it_be(:poll) { Fabricate.build(:poll) }
+      let(:poll) { Fabricate(:poll) }
 
       it 'marks vote invalid with negative choice' do
         vote = Fabricate.build(:poll_vote, poll: poll, choice: -100)
@@ -30,15 +30,15 @@ RSpec.describe PollVote do
       end
 
       it 'marks vote invalid with choice in excess of options' do
-        poll.options = %w(a b c)
+        poll.update(options: %w(a b c))
         vote = Fabricate.build(:poll_vote, poll: poll, choice: 10)
         expect(vote).to_not be_valid
       end
     end
 
     context 'with a poll where multiple is true' do
-      let_it_be(:poll) { Fabricate(:poll, multiple: true, options: %w(a b c)) }
-      let_it_be(:first_vote) { Fabricate(:poll_vote, poll: poll, choice: 1) }
+      let(:poll) { Fabricate(:poll, multiple: true, options: %w(a b c)) }
+      let(:first_vote) { Fabricate(:poll_vote, poll: poll, choice: 1) }
 
       it 'does not allow a second vote on same choice from same account' do
         expect(first_vote).to be_valid
@@ -49,8 +49,8 @@ RSpec.describe PollVote do
     end
 
     context 'with a poll where multiple is false' do
-      let_it_be(:poll) { Fabricate(:poll, multiple: false, options: %w(a b c)) }
-      let_it_be(:first_vote) { Fabricate(:poll_vote, poll: poll) }
+      let(:poll) { Fabricate(:poll, multiple: false, options: %w(a b c)) }
+      let(:first_vote) { Fabricate(:poll_vote, poll: poll) }
 
       it 'does not allow a second vote from same account' do
         expect(first_vote).to be_valid
