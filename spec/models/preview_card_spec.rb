53b392e36e3f2dbdb46f451d1091de9ae6867b30
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 21:12:53 +0100

    RUN_ID: 2

--- spec/models/preview_card_spec.rb
+++ spec/models/preview_card_spec.rb
@@ -2,25 +2,26 @@
 
 require 'rails_helper'
 
-RSpec.describe PreviewCard do
-  let_it_be(:preview_card) { described_class.new }
-
+describe PreviewCard do
   describe 'validations' do
     describe 'urls' do
       it 'allows http schemes' do
-        preview_card.url = 'http://example.host/path'
-        expect(preview_card).to be_valid
+        record = described_class.new(url: 'http://example.host/path')
+
+        expect(record).to be_valid
       end
 
       it 'allows https schemes' do
-        preview_card.url = 'https://example.host/path'
-        expect(preview_card).to be_valid
+        record = described_class.new(url: 'https://example.host/path')
+
+        expect(record).to be_valid
       end
 
       it 'does not allow javascript: schemes' do
-        preview_card.url = 'javascript:alert()'
-        expect(preview_card).to_not be_valid
-        expect(preview_card).to model_have_error_on_field(:url)
+        record = described_class.new(url: 'javascript:alert()')
+
+        expect(record).to_not be_valid
+        expect(record).to model_have_error_on_field(:url)
       end
     end
   end
