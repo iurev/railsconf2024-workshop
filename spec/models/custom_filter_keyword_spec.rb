76a140ff1de7b987f6069b8ac33ec3cacfca9fd0
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 19:41:50 +0100

    RUN_ID: 1

--- spec/models/custom_filter_keyword_spec.rb
+++ spec/models/custom_filter_keyword_spec.rb
@@ -1,35 +1,32 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 RSpec.describe CustomFilterKeyword do
   describe '#to_regex' do
     context 'when whole_word is true' do
-      it 'builds a regex with boundaries and the keyword' do
-        keyword = described_class.new(whole_word: true, keyword: 'test')
+      let_it_be(:keyword_whole_word) { described_class.new(whole_word: true, keyword: 'test') }
+      let_it_be(:keyword_end_non_word) { described_class.new(whole_word: true, keyword: 'test#') }
+      let_it_be(:keyword_start_non_word) { described_class.new(whole_word: true, keyword: '#test') }
 
-        expect(keyword.to_regex).to eq(/(?mix:\b#{Regexp.escape(keyword.keyword)}\b)/)
+      it 'builds a regex with boundaries and the keyword' do
+        expect(keyword_whole_word.to_regex).to eq(/(?mix:\b#{Regexp.escape(keyword_whole_word.keyword)}\b)/)
       end
 
       it 'builds a regex with starting boundary and the keyword when end with non-word' do
-        keyword = described_class.new(whole_word: true, keyword: 'test#')
-
-        expect(keyword.to_regex).to eq(/(?mix:\btest\#)/)
+        expect(keyword_end_non_word.to_regex).to eq(/(?mix:\btest\#)/)
       end
 
       it 'builds a regex with end boundary and the keyword when start with non-word' do
-        keyword = described_class.new(whole_word: true, keyword: '#test')
-
-        expect(keyword.to_regex).to eq(/(?mix:\#test\b)/)
+        expect(keyword_start_non_word.to_regex).to eq(/(?mix:\#test\b)/)
       end
     end
 
     context 'when whole_word is false' do
-      it 'builds a regex with the keyword' do
-        keyword = described_class.new(whole_word: false, keyword: 'test')
+      let_it_be(:keyword_not_whole_word) { described_class.new(whole_word: false, keyword: 'test') }
 
-        expect(keyword.to_regex).to eq(/test/i)
+      it 'builds a regex with the keyword' do
+        expect(keyword_not_whole_word.to_regex).to eq(/test/i)
       end
     end
   end
