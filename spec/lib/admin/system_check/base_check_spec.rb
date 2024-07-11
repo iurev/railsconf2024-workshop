73822c8433035f0cdbcc341456bb3513c528a12e
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 14:18:01 +0100

    RUN_ID: 1

--- spec/lib/admin/system_check/base_check_spec.rb
+++ spec/lib/admin/system_check/base_check_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe Admin::SystemCheck::BaseCheck do
   subject(:check) { described_class.new(user) }
 
-  let(:user) { Fabricate(:user) }
+  let_it_be(:user) { Fabricate(:user) }
 
   describe 'skip?' do
     it 'returns false' do
