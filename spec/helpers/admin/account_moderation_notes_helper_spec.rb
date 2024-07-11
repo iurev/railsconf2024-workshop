7264caa23eeac50c1c980327ccf80e4f93a896cb
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 09:35:33 +0100

    RUN_ID: 1

--- spec/helpers/admin/account_moderation_notes_helper_spec.rb
+++ spec/helpers/admin/account_moderation_notes_helper_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -16,7 +15,7 @@ RSpec.describe Admin::AccountModerationNotesHelper do
     end
 
     context 'with account' do
-      let(:account) { Fabricate(:account) }
+      let_it_be(:account) { Fabricate(:account) }
 
       it 'calls #link_to' do
         allow(helper).to receive(:link_to)
@@ -42,7 +41,7 @@ RSpec.describe Admin::AccountModerationNotesHelper do
     end
 
     context 'with account' do
-      let(:account) { Fabricate(:account) }
+      let_it_be(:account) { Fabricate(:account) }
 
       it 'calls #link_to' do
         result = helper.admin_account_inline_link_to(account)
