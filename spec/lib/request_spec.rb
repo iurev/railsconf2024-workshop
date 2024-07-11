fe7067c8a1223b37de9ce8df5bdff2dd81b2e344
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 16:29:49 +0100

    RUN_ID: 1

--- spec/lib/request_spec.rb
+++ spec/lib/request_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 require 'securerandom'
 
 describe Request do
-  subject { described_class.new(:get, 'http://example.com') }
+  let_it_be(:subject) { described_class.new(:get, 'http://example.com') }
 
   describe '#headers' do
     it 'returns user agent' do
