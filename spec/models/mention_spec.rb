c4c9bdf468460fe2369b207bb3907ee388350fb6
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 21:04:42 +0100

    RUN_ID: 1

--- spec/models/mention_spec.rb
+++ spec/models/mention_spec.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 RSpec.describe Mention do
+  let_it_be(:account) { Fabricate(:account) }
+  let_it_be(:status) { Fabricate(:status) }
+
   describe 'validations' do
     it 'is invalid without an account' do
-      mention = Fabricate.build(:mention, account: nil)
+      mention = Fabricate.build(:mention, account: nil, status: status)
       mention.valid?
       expect(mention).to model_have_error_on_field(:account)
     end
 
     it 'is invalid without a status' do
-      mention = Fabricate.build(:mention, status: nil)
+      mention = Fabricate.build(:mention, account: account, status: nil)
       mention.valid?
       expect(mention).to model_have_error_on_field(:status)
     end
