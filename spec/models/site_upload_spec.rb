4b9fef2dd0cf55ea48d61b062d79c40696d2382c
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 21:55:04 +0100

    RUN_ID: 1

--- spec/models/site_upload_spec.rb
+++ spec/models/site_upload_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 RSpec.describe SiteUpload do
   describe '#cache_key' do
-    let(:site_upload) { described_class.new(var: 'var') }
+    let_it_be(:site_upload) { described_class.new(var: 'var') }
 
     it 'returns cache_key' do
       expect(site_upload.cache_key).to eq 'site_uploads/var'
