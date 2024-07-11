e6674d4050879a783d5593c72a0ba0936634129f
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 23:09:45 +0100

    RUN_ID: 1

--- spec/policies/account_warning_preset_policy_spec.rb
+++ spec/policies/account_warning_preset_policy_spec.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 require 'pundit/rspec'
 
 describe AccountWarningPresetPolicy do
   let(:policy) { described_class }
-  let(:admin)   { Fabricate(:user, role: UserRole.find_by(name: 'Admin')).account }
-  let(:john)    { Fabricate(:account) }
+  
+  before_all do
+    UserRole.find_or_create_by(name: 'Admin')
+  end
+
+  let_it_be(:admin) { Fabricate(:user, role: UserRole.find_by(name: 'Admin')).account }
+  let_it_be(:john)  { Fabricate(:account) }
 
   permissions :index?, :create?, :update?, :destroy? do
     context 'with an admin' do
