915845807864ed5228781e70c823ebd0691f3732
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 04:37:14 +0100

    RUN_ID: 1

--- spec/controllers/concerns/settings/export_controller_concern_spec.rb
+++ spec/controllers/concerns/settings/export_controller_concern_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -16,9 +15,10 @@ describe Settings::ExportControllerConcern do
     end
   end
 
+  let_it_be(:user) { Fabricate(:user) }
+
   describe 'GET #index' do
     it 'returns a csv of the exported data when signed in' do
-      user = Fabricate(:user)
       sign_in user
       get :index, format: :csv
 
