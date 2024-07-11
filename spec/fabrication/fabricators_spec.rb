f687db5dc078ab56689c3c815acef8cbd0225a22
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 07:58:36 +0100

    RUN_ID: 4

--- spec/fabrication/fabricators_spec.rb
+++ spec/fabrication/fabricators_spec.rb
@@ -2,14 +2,20 @@
 
 require 'rails_helper'
 
-RSpec.describe "Fabricators" do
+RSpec.configure do |config|
+  config.define_derived_metadata(file_path: %r{/spec/fabrication/}) do |metadata|
+    metadata[:type] = :fabrication
+  end
+end
+
+RSpec.describe "Fabricators", type: :fabrication do
   before(:all) do
     Fabrication.manager.load_definitions if Fabrication.manager.empty?
   end
 
   Fabrication.manager.schematics.map(&:first).each do |factory_name|
     describe "The #{factory_name} factory" do
-      it 'is able to create valid records', fabrication: true do
+      it 'is able to create valid records' do
         records = Fabricate.times(2, factory_name)
         expect(records).to all(be_valid)
       end
