513640065222a612077ae7f35d969bc52b5024a6
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 09:20:20 +0100

    RUN_ID: 1

--- spec/features/captcha_spec.rb
+++ spec/features/captcha_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 describe 'email confirmation flow when captcha is enabled' do
-  let(:user)        { Fabricate(:user, confirmed_at: nil, confirmation_token: 'foobar', created_by_application: client_app) }
-  let(:client_app)  { nil }
+  let_it_be(:user) { Fabricate(:user, confirmed_at: nil, confirmation_token: 'foobar', created_by_application: client_app) }
+  let(:client_app) { nil }
 
   before do
     allow(Auth::ConfirmationsController).to receive(:new).and_return(stubbed_controller)
