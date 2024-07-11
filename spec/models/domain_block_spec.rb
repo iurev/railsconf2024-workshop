4915d6145b911840710cea5a29d51bd12ca66d2d
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 19:45:59 +0100

    RUN_ID: 3

--- spec/models/domain_block_spec.rb
+++ spec/models/domain_block_spec.rb
@@ -5,17 +5,19 @@ require 'rails_helper'
 RSpec.describe DomainBlock do
   let_it_be(:suspend_block) { Fabricate(:domain_block, domain: 'suspended.com', severity: :suspend) }
   let_it_be(:silence_block) { Fabricate(:domain_block, domain: 'silenced.com', severity: :silence) }
+  let_it_be(:subdomain_block) { Fabricate(:domain_block, domain: 'sub.example.com') }
+  let_it_be(:tld_block) { Fabricate(:domain_block, domain: 'google') }
 
   describe 'validations' do
     it 'is invalid without a domain' do
-      domain_block = Fabricate.build(:domain_block, domain: nil)
+      domain_block = build(:domain_block, domain: nil)
       domain_block.valid?
       expect(domain_block).to model_have_error_on_field(:domain)
     end
 
     it 'is invalid if the same normalized domain already exists' do
-      Fabricate(:domain_block, domain: 'にゃん')
-      domain_block_with_normalized_value = Fabricate.build(:domain_block, domain: 'xn--r9j5b5b')
+      create(:domain_block, domain: 'にゃん')
+      domain_block_with_normalized_value = build(:domain_block, domain: 'xn--r9j5b5b')
       domain_block_with_normalized_value.valid?
       expect(domain_block_with_normalized_value).to model_have_error_on_field(:domain)
     end
@@ -36,9 +38,6 @@ RSpec.describe DomainBlock do
   end
 
   describe '.rule_for' do
-    let_it_be(:subdomain_block) { Fabricate(:domain_block, domain: 'sub.example.com') }
-    let_it_be(:tld_block) { Fabricate(:domain_block, domain: 'google') }
-
     it 'returns rule matching a blocked domain' do
       expect(described_class.rule_for('suspended.com')).to eq suspend_block
     end
@@ -61,7 +60,7 @@ RSpec.describe DomainBlock do
   end
 
   describe '#stricter_than?' do
-    let(:noop) { described_class.new(domain: 'noop.com', severity: :noop) }
+    let(:noop) { build(:domain_block, domain: 'noop.com', severity: :noop) }
 
     it 'returns true if the new block has suspend severity while the old has lower severity' do
       expect(suspend_block.stricter_than?(silence_block)).to be true
@@ -75,33 +74,27 @@ RSpec.describe DomainBlock do
     end
 
     it 'returns false if the new block is less strict regarding reports' do
-      older = described_class.new(domain: 'older.com', severity: :silence, reject_reports: true)
-      newer = described_class.new(domain: 'newer.com', severity: :silence, reject_reports: false)
+      older = build(:domain_block, domain: 'older.com', severity: :silence, reject_reports: true)
+      newer = build(:domain_block, domain: 'newer.com', severity: :silence, reject_reports: false)
       expect(newer.stricter_than?(older)).to be false
     end
 
     it 'returns false if the new block is less strict regarding media' do
-      older = described_class.new(domain: 'older.com', severity: :silence, reject_media: true)
-      newer = described_class.new(domain: 'newer.com', severity: :silence, reject_media: false)
+      older = build(:domain_block, domain: 'older.com', severity: :silence, reject_media: true)
+      newer = build(:domain_block, domain: 'newer.com', severity: :silence, reject_media: false)
       expect(newer.stricter_than?(older)).to be false
     end
   end
 
   describe '#public_domain' do
-    context 'with a domain block that is obfuscated' do
-      let(:domain_block) { Fabricate(:domain_block, domain: 'hostname.example.com', obfuscate: true) }
-
-      it 'garbles the domain' do
-        expect(domain_block.public_domain).to eq 'hostna**.******e.com'
-      end
+    it 'garbles the domain when obfuscated' do
+      domain_block = build(:domain_block, domain: 'hostname.example.com', obfuscate: true)
+      expect(domain_block.public_domain).to eq 'hostna**.******e.com'
     end
 
-    context 'with a domain block that is not obfuscated' do
-      let(:domain_block) { Fabricate(:domain_block, domain: 'example.com', obfuscate: false) }
-
-      it 'returns the domain value' do
-        expect(domain_block.public_domain).to eq 'example.com'
-      end
+    it 'returns the domain value when not obfuscated' do
+      domain_block = build(:domain_block, domain: 'example.com', obfuscate: false)
+      expect(domain_block.public_domain).to eq 'example.com'
     end
   end
 end
