bdf9c6719e295d71fecced22248514be3827a51d
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 23:08:00 +0100

    RUN_ID: 1

--- spec/policies/account_moderation_note_policy_spec.rb
+++ spec/policies/account_moderation_note_policy_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 require 'pundit/rspec'
@@ -7,8 +6,8 @@ require 'pundit/rspec'
 RSpec.describe AccountModerationNotePolicy do
   subject { described_class }
 
-  let(:admin)   { Fabricate(:user, role: UserRole.find_by(name: 'Admin')).account }
-  let(:john)    { Fabricate(:account) }
+  let_it_be(:admin) { Fabricate(:user, role: UserRole.find_by(name: 'Admin')).account }
+  let_it_be(:john)  { Fabricate(:account) }
 
   permissions :create? do
     context 'when staff' do
@@ -25,7 +24,7 @@ RSpec.describe AccountModerationNotePolicy do
   end
 
   permissions :destroy? do
-    let(:account_moderation_note) do
+    let_it_be(:account_moderation_note) do
       Fabricate(:account_moderation_note,
                 account: john,
                 target_account: Fabricate(:account))
@@ -44,7 +43,7 @@ RSpec.describe AccountModerationNotePolicy do
     end
 
     context 'when neither admin nor owner' do
-      let(:kevin) { Fabricate(:account) }
+      let_it_be(:kevin) { Fabricate(:account) }
 
       it 'denies to destroy' do
         expect(subject).to_not permit(kevin, account_moderation_note)
