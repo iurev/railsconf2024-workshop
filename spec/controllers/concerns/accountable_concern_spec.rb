890840e64b6e0c2f207d099a67746c965f3d002a
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 04:01:55 +0100

    RUN_ID: 1

--- spec/controllers/concerns/accountable_concern_spec.rb
+++ spec/controllers/concerns/accountable_concern_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -15,8 +14,8 @@ RSpec.describe AccountableConcern do
     end
   end
 
-  let(:user)   { Fabricate(:account) }
-  let(:target) { Fabricate(:account) }
+  let_it_be(:user)   { Fabricate(:account) }
+  let_it_be(:target) { Fabricate(:account) }
   let(:hoge)   { hoge_class.new(user) }
 
   describe '#log_action' do
