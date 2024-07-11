ea548dc9f2cecbfa44d46f883cd05dce66d76290
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 19:08:22 +0100

    RUN_ID: 1

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 RSpec.describe Conversation do
+  let_it_be(:conversation) { Fabricate(:conversation) }
+
   describe '#local?' do
     it 'returns true when URI is nil' do
-      expect(Fabricate(:conversation).local?).to be true
+      expect(conversation.local?).to be true
     end
 
     it 'returns false when URI is not nil' do
-      expect(Fabricate(:conversation, uri: 'abc').local?).to be false
+      conversation.update(uri: 'abc')
+      expect(conversation.local?).to be false
     end
   end
 end
