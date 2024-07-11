3ab9e4ca8ef60d192f98c93556d8c9c22d6413ea
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-11 19:43:59 +0100

    RUN_ID: 1

--- spec/models/domain_allow_spec.rb
+++ spec/models/domain_allow_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
-describe DomainAllow do
+RSpec.describe DomainAllow do
   describe 'Validations' do
     it 'is invalid without a domain' do
       domain_allow = Fabricate.build(:domain_allow, domain: nil)
@@ -12,7 +11,8 @@ describe DomainAllow do
     end
 
     it 'is invalid if the same normalized domain already exists' do
-      _domain_allow = Fabricate(:domain_allow, domain: 'にゃん')
+      let_it_be(:existing_domain_allow) { Fabricate(:domain_allow, domain: 'にゃん') }
+      
       domain_allow_with_normalized_value = Fabricate.build(:domain_allow, domain: 'xn--r9j5b5b')
       domain_allow_with_normalized_value.valid?
       expect(domain_allow_with_normalized_value).to model_have_error_on_field(:domain)
