9673fce6cbdadead462368207fb572ca2398c5ac
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-12 01:51:40 +0100

    RUN_ID: 2

--- spec/requests/api/v1/admin/trends/links/preview_card_providers_spec.rb
+++ spec/requests/api/v1/admin/trends/links/preview_card_providers_spec.rb
@@ -6,17 +6,15 @@ describe 'API V1 Admin Trends Links Preview Card Providers' do
   let_it_be(:role)   { UserRole.find_by(name: 'Admin') }
   let_it_be(:user)   { Fabricate(:user, role: role) }
   let_it_be(:scopes) { 'admin:read admin:write' }
-  let_it_be(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
   let_it_be(:account) { Fabricate(:account) }
   let_it_be(:preview_card_provider) { Fabricate(:preview_card_provider) }
 
-  before_all do
-    @headers = { 'Authorization' => "Bearer #{token.token}" }
-  end
+  let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+  let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
   describe 'GET /api/v1/admin/trends/links/publishers' do
     it 'returns http success' do
-      get '/api/v1/admin/trends/links/publishers', params: { account_id: account.id, limit: 2 }, headers: @headers
+      get '/api/v1/admin/trends/links/publishers', params: { account_id: account.id, limit: 2 }, headers: headers
 
       expect(response).to have_http_status(200)
     end
@@ -24,7 +22,7 @@ describe 'API V1 Admin Trends Links Preview Card Providers' do
 
   describe 'POST /api/v1/admin/trends/links/publishers/:id/approve' do
     before do
-      post "/api/v1/admin/trends/links/publishers/#{preview_card_provider.id}/approve", headers: @headers
+      post "/api/v1/admin/trends/links/publishers/#{preview_card_provider.id}/approve", headers: headers
     end
 
     it_behaves_like 'forbidden for wrong scope', 'write:statuses'
@@ -37,7 +35,7 @@ describe 'API V1 Admin Trends Links Preview Card Providers' do
 
   describe 'POST /api/v1/admin/trends/links/publishers/:id/reject' do
     before do
-      post "/api/v1/admin/trends/links/publishers/#{preview_card_provider.id}/reject", headers: @headers
+      post "/api/v1/admin/trends/links/publishers/#{preview_card_provider.id}/reject", headers: headers
     end
 
     it_behaves_like 'forbidden for wrong scope', 'write:statuses'
