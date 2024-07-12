a7cd3e22af6bccfb00a0bd87d26337591b075526
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-12 03:18:09 +0100

    RUN_ID: 1

--- spec/requests/api/v1/notifications/policies_spec.rb
+++ spec/requests/api/v1/notifications/policies_spec.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 RSpec.describe 'Policies' do
-  let(:user)    { Fabricate(:user, account_attributes: { username: 'alice' }) }
-  let(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
-  let(:scopes)  { 'read:notifications write:notifications' }
+  let_it_be(:user) { Fabricate(:user, account_attributes: { username: 'alice' }) }
+  let_it_be(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'read:notifications write:notifications') }
   let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
   describe 'GET /api/v1/notifications/policy' do
