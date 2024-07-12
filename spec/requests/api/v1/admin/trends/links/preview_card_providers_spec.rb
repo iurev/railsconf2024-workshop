4dbb9f378350924431a162bb49ac251791a627b8
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-12 01:51:57 +0100

    RUN_ID: 3

--- spec/requests/api/v1/admin/trends/links/preview_card_providers_spec.rb
+++ spec/requests/api/v1/admin/trends/links/preview_card_providers_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 describe 'API V1 Admin Trends Links Preview Card Providers' do
-  let_it_be(:role)   { UserRole.find_by(name: 'Admin') }
-  let_it_be(:user)   { Fabricate(:user, role: role) }
+  let_it_be(:admin_role) { UserRole.find_by(name: 'Admin') }
+  let_it_be(:user)   { Fabricate(:user, role: admin_role) }
   let_it_be(:scopes) { 'admin:read admin:write' }
   let_it_be(:account) { Fabricate(:account) }
   let_it_be(:preview_card_provider) { Fabricate(:preview_card_provider) }
@@ -21,28 +21,44 @@ describe 'API V1 Admin Trends Links Preview Card Providers' do
   end
 
   describe 'POST /api/v1/admin/trends/links/publishers/:id/approve' do
-    before do
-      post "/api/v1/admin/trends/links/publishers/#{preview_card_provider.id}/approve", headers: headers
+    context 'with correct permissions' do
+      before do
+        post "/api/v1/admin/trends/links/publishers/#{preview_card_provider.id}/approve", headers: headers
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(200)
+      end
     end
 
-    it_behaves_like 'forbidden for wrong scope', 'write:statuses'
-    it_behaves_like 'forbidden for wrong role', ''
+    it_behaves_like 'forbidden for wrong scope', 'write:statuses' do
+      let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'write:statuses') }
+    end
 
-    it 'returns http success' do
-      expect(response).to have_http_status(200)
+    it_behaves_like 'forbidden for wrong role', '' do
+      let(:user_role) { UserRole.find_by(name: 'User') }
+      let(:user) { Fabricate(:user, role: user_role) }
     end
   end
 
   describe 'POST /api/v1/admin/trends/links/publishers/:id/reject' do
-    before do
-      post "/api/v1/admin/trends/links/publishers/#{preview_card_provider.id}/reject", headers: headers
+    context 'with correct permissions' do
+      before do
+        post "/api/v1/admin/trends/links/publishers/#{preview_card_provider.id}/reject", headers: headers
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(200)
+      end
     end
 
-    it_behaves_like 'forbidden for wrong scope', 'write:statuses'
-    it_behaves_like 'forbidden for wrong role', ''
+    it_behaves_like 'forbidden for wrong scope', 'write:statuses' do
+      let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'write:statuses') }
+    end
 
-    it 'returns http success' do
-      expect(response).to have_http_status(200)
+    it_behaves_like 'forbidden for wrong role', '' do
+      let(:user_role) { UserRole.find_by(name: 'User') }
+      let(:user) { Fabricate(:user, role: user_role) }
     end
   end
 end
