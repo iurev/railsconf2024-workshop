ca3dcd7a76567c3a4cb38d69a6be25660e75d17e
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-12 04:36:16 +0100

    RUN_ID: 1

--- spec/requests/api/v1/trends/tags_spec.rb
+++ spec/requests/api/v1/trends/tags_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
@@ -19,20 +18,21 @@ RSpec.describe 'API V1 Trends Tags' do
     context 'when trends are enabled' do
       before { Setting.trends = true }
 
+      let_it_be(:tags) { Fabricate.times(3, :tag, trendable: true) }
+
+      before do
+        tags.each do |tag|
+          2.times { |i| Trends.tags.add(tag, i) }
+        end
+        Trends::Tags.new(threshold: 1).refresh
+      end
+
       it 'returns http success' do
-        prepare_trends
         stub_const('Api::V1::Trends::TagsController::DEFAULT_TAGS_LIMIT', 2)
         get '/api/v1/trends/tags'
 
         expect(response).to have_http_status(200)
         expect(response.headers).to include('Link')
-      end
-
-      def prepare_trends
-        Fabricate.times(3, :tag, trendable: true).each do |tag|
-          2.times { |i| Trends.tags.add(tag, i) }
-        end
-        Trends::Tags.new(threshold: 1).refresh
       end
     end
   end
