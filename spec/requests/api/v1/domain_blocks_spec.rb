65e8b5bea7861c595eb4f35a1e3f4c6773809707
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-12 02:31:51 +0100

    RUN_ID: 3

--- spec/requests/api/v1/domain_blocks_spec.rb
+++ spec/requests/api/v1/domain_blocks_spec.rb
@@ -3,11 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Domain blocks' do
-  let_it_be(:user)    { Fabricate(:user) }
-  let_it_be(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'read:blocks') }
-  let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+  let_it_be(:user) { Fabricate(:user) }
 
   describe 'GET /api/v1/domain_blocks' do
+    let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+    let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+    let(:scopes) { 'read:blocks' }
+
     subject do
       get '/api/v1/domain_blocks', headers: headers, params: params
     end
@@ -21,69 +23,79 @@ RSpec.describe 'Domain blocks' do
 
     it_behaves_like 'forbidden for wrong scope', 'write:blocks'
 
-    it 'returns the domains blocked by the requesting user', :aggregate_failures do
-      subject
-
-      expect(response).to have_http_status(200)
-      expect(body_as_json).to match_array(blocked_domains)
-    end
-
-    context 'with limit param' do
-      let(:params) { { limit: 2 } }
-
-      it 'returns only the requested number of blocked domains' do
+    context 'with correct scope' do
+      it 'returns the domains blocked by the requesting user', :aggregate_failures do
         subject
 
-        expect(body_as_json.size).to eq(params[:limit])
+        expect(response).to have_http_status(200)
+        expect(body_as_json).to match_array(blocked_domains)
+      end
+
+      context 'with limit param' do
+        let(:params) { { limit: 2 } }
+
+        it 'returns only the requested number of blocked domains' do
+          subject
+
+          expect(body_as_json.size).to eq(params[:limit])
+        end
       end
     end
   end
 
   describe 'POST /api/v1/domain_blocks' do
+    let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+    let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+    let(:scopes) { 'write:blocks' }
+
     subject do
       post '/api/v1/domain_blocks', headers: headers, params: params
     end
 
     let(:params) { { domain: 'example.com' } }
-    let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'write:blocks') }
 
     it_behaves_like 'forbidden for wrong scope', 'read:blocks'
 
-    it 'creates a domain block', :aggregate_failures do
-      subject
-
-      expect(response).to have_http_status(200)
-      expect(user.account.domain_blocking?(params[:domain])).to be(true)
-    end
-
-    context 'when no domain name is given' do
-      let(:params) { { domain: '' } }
-
-      it 'returns http unprocessable entity' do
+    context 'with correct scope' do
+      it 'creates a domain block', :aggregate_failures do
         subject
 
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(200)
+        expect(user.account.domain_blocking?(params[:domain])).to be(true)
       end
-    end
 
-    context 'when the given domain name is invalid' do
-      let(:params) { { domain: 'example com' } }
+      context 'when no domain name is given' do
+        let(:params) { { domain: '' } }
 
-      it 'returns unprocessable entity' do
-        subject
+        it 'returns http unprocessable entity' do
+          subject
 
-        expect(response).to have_http_status(422)
+          expect(response).to have_http_status(422)
+        end
+      end
+
+      context 'when the given domain name is invalid' do
+        let(:params) { { domain: 'example com' } }
+
+        it 'returns unprocessable entity' do
+          subject
+
+          expect(response).to have_http_status(422)
+        end
       end
     end
   end
 
   describe 'DELETE /api/v1/domain_blocks' do
+    let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+    let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+    let(:scopes) { 'write:blocks' }
+
     subject do
       delete '/api/v1/domain_blocks', headers: headers, params: params
     end
 
     let(:params) { { domain: 'example.com' } }
-    let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'write:blocks') }
 
     before do
       user.account.block_domain!('example.com')
@@ -91,20 +103,22 @@ RSpec.describe 'Domain blocks' do
 
     it_behaves_like 'forbidden for wrong scope', 'read:blocks'
 
-    it 'deletes the specified domain block', :aggregate_failures do
-      subject
-
-      expect(response).to have_http_status(200)
-      expect(user.account.domain_blocking?('example.com')).to be(false)
-    end
-
-    context 'when the given domain name is not blocked' do
-      let(:params) { { domain: 'example.org' } }
-
-      it 'returns http success' do
+    context 'with correct scope' do
+      it 'deletes the specified domain block', :aggregate_failures do
         subject
 
         expect(response).to have_http_status(200)
+        expect(user.account.domain_blocking?('example.com')).to be(false)
+      end
+
+      context 'when the given domain name is not blocked' do
+        let(:params) { { domain: 'example.org' } }
+
+        it 'returns http success' do
+          subject
+
+          expect(response).to have_http_status(200)
+        end
       end
     end
   end
