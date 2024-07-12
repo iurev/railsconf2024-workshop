e72c72fe34157282c06e97a31a95911b20e01759
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-12 02:35:28 +0100

    RUN_ID: 1

--- spec/requests/api/v1/filters_spec.rb
+++ spec/requests/api/v1/filters_spec.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
-# aiptimize started
 
 require 'rails_helper'
 
 RSpec.describe 'API V1 Filters' do
-  let(:user)    { Fabricate(:user) }
-  let(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+  let_it_be(:user) { Fabricate(:user) }
+  let_it_be(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id) }
   let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
   describe 'GET /api/v1/filters' do
-    let(:scopes) { 'read:filters' }
-    let!(:filter) { Fabricate(:custom_filter, account: user.account) }
-    let!(:custom_filter_keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+    let_it_be(:filter) { Fabricate(:custom_filter, account: user.account) }
+    let_it_be(:custom_filter_keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+
+    before_all do
+      token.update!(scopes: 'read:filters')
+    end
 
     it 'returns http success' do
       get '/api/v1/filters', headers: headers
@@ -24,7 +26,10 @@ RSpec.describe 'API V1 Filters' do
   end
 
   describe 'POST /api/v1/filters' do
-    let(:scopes) { 'write:filters' }
+    before_all do
+      token.update!(scopes: 'write:filters')
+    end
+
     let(:irreversible) { true }
     let(:whole_word)   { false }
 
@@ -61,9 +66,12 @@ RSpec.describe 'API V1 Filters' do
   end
 
   describe 'GET /api/v1/filters/:id' do
-    let(:scopes)  { 'read:filters' }
-    let(:filter)  { Fabricate(:custom_filter, account: user.account) }
-    let(:keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+    let_it_be(:filter)  { Fabricate(:custom_filter, account: user.account) }
+    let_it_be(:keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+
+    before_all do
+      token.update!(scopes: 'read:filters')
+    end
 
     it 'returns http success' do
       get "/api/v1/filters/#{keyword.id}", headers: headers
@@ -73,9 +81,12 @@ RSpec.describe 'API V1 Filters' do
   end
 
   describe 'PUT /api/v1/filters/:id' do
-    let(:scopes)  { 'write:filters' }
-    let(:filter)  { Fabricate(:custom_filter, account: user.account) }
-    let(:keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+    let_it_be(:filter)  { Fabricate(:custom_filter, account: user.account) }
+    let_it_be(:keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+
+    before_all do
+      token.update!(scopes: 'write:filters')
+    end
 
     before do
       put "/api/v1/filters/#{keyword.id}", headers: headers, params: { phrase: 'updated' }
@@ -88,9 +99,12 @@ RSpec.describe 'API V1 Filters' do
   end
 
   describe 'DELETE /api/v1/filters/:id' do
-    let(:scopes)  { 'write:filters' }
-    let(:filter)  { Fabricate(:custom_filter, account: user.account) }
-    let(:keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+    let_it_be(:filter)  { Fabricate(:custom_filter, account: user.account) }
+    let_it_be(:keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+
+    before_all do
+      token.update!(scopes: 'write:filters')
+    end
 
     before do
       delete "/api/v1/filters/#{keyword.id}", headers: headers
