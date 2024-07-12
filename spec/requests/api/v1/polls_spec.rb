baa4acff4b37b8e7fe0355350992c6110359fdc8
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-12 03:50:53 +0100

    RUN_ID: 2

--- spec/requests/api/v1/polls_spec.rb
+++ spec/requests/api/v1/polls_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Polls' do
   let_it_be(:user)    { Fabricate(:user) }
+  let(:scopes)  { 'read:statuses' }
   let_it_be(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'read:statuses') }
   let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
