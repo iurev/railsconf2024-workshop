e4b0a8015b6679ed11ee519bf632d7d7bffa2f18
Vitalii Yulieff <wwjurew@gmail.com>
2024-07-12 03:18:24 +0100

    RUN_ID: 2

--- spec/requests/api/v1/notifications/policies_spec.rb
+++ spec/requests/api/v1/notifications/policies_spec.rb
@@ -4,7 +4,8 @@ require 'rails_helper'
 
 RSpec.describe 'Policies' do
   let_it_be(:user) { Fabricate(:user, account_attributes: { username: 'alice' }) }
-  let_it_be(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'read:notifications write:notifications') }
+  let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+  let(:scopes) { 'read:notifications write:notifications' }
   let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
   describe 'GET /api/v1/notifications/policy' do
